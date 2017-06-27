41b87eba982cf6235b9e8ef24c8fb17dc306c5a7
kokh0716 <konstantin.kharlambov@netcracker.com>
2017-06-27 13:26:25 +0300

    Header was replaced

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by kokh0716 on 6/27/2017.
+ */
+export * from "./src/lib/core-module/core-module"
+export * from "./src/lib/validation-module/validation-module"
--- src/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Created by kokh0716 on 6/27/2017.
- */
-export * from "./core-module/core-module"
-export * from "./validation-module/validation-module"
