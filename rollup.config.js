9aa6039dc76ed3a0d32c7dc9b0249f85dae88e9b
kokh0716 <konstantin.kharlambov@netcracker.com>
2017-08-21 18:21:11 +0300

    Uglify was added

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,11 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import builtins from 'rollup-plugin-node-builtins';
+import uglify from 'rollup-plugin-uglify-es';
 
 export default {
     entry: './src/index.ts',
-    dest: 'index.js',
+    dest: 'index.min.js',
     format: 'cjs',
     external: ['lodash', 'util'],
     plugins: [
@@ -17,6 +18,19 @@ export default {
         commonjs({
             extensions: ['.js', '.ts']
         }),
-        builtins()
+        builtins(),
+        uglify({
+            mangle: true,
+            compress: {
+                sequences: true,
+                dead_code: true,
+                conditionals: true,
+                booleans: true,
+                unused: true,
+                if_return: true,
+                join_vars: true,
+                drop_console: true
+            }
+        })
     ]
 }
